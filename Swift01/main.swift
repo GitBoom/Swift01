d5efc7ad8347031cb081b76b252748751105da68
wuhao <a20131105803@mac21.local>
2016-03-07 19:55:09 +0800

    Initial Commit

---
+++ Swift01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift01
+//
+//  Created by 20131105803 on 16/3/7.
+//  Copyright © 2016年 20131105803. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
